82d9279ad314a265dad834982f6e6e88923bdcec
Muneeb <muneebhasan803@gmail.com>
2023-07-05 14:30:51 -0400

    new changes

--- Hello.c
+++ Hello.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int x = 6;
+    int x = 7;
     printf(" Original value : %d\n", x);
     printf(" After x ++: %d\n", x++);    // Postfix increment
     printf(" After ++x: %d\n", ++x);     // Prefix increment
